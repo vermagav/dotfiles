ede70ef62f9574da30ebc94a41dcf8c8b6c84c3d
Gav Verma <gverma@uber.com>
2015-12-10 14:24:45 -0800

    Added script to install atom packages

---
+++ atom_packages.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This script installs the following Atom packages. Requires Atom.
+
+apm install go-plus
+apm install highlight-selected
+apm install linter
+apm install linter-flake8 # python linter
+apm install minimap
+apm install save-session
+apm install sort-lines
