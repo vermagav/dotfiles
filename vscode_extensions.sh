788de716456b70293d24466c97b7366a3e6befbe
Gav Verma <gverma@uber.com>
2017-06-20 12:59:23 -0700

    Add script to install vs-code extensions

---
+++ vscode_extensions.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This script installs the following VS Code extensions. Requires VS Code.
+# To view details, go to `https://marketplace.visualstudio.com/items?itemName=<extension>`.
+
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension donjayamanne.python
+code --install-extension jaysonsantos.vscode-flake8
+code --install-extension lukehoban.Go
+code --install-extension mohsen1.prettify-json
+code --install-extension ms-vscode.cpptools
+code --install-extension robertohuertasm.vscode-icons
